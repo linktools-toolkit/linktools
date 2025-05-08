11cb7d07422a9da8c230dfa49b0772cc54ca94dc
huji <669898595@qq.com>
2025-05-08 18:42:03 +0800

    update develop flag

--- setup.py
+++ setup.py
@@ -28,20 +28,14 @@
 """
 import json
 import os
+import pathlib
 import pkgutil
 import re
 
 import yaml
 from jinja2 import Template
 from setuptools import setup
-
-
-def get_root_path(*paths):
-    return os.path.join(os.path.abspath(os.path.dirname(__file__)), *paths)
-
-
-def get_src_path(*paths):
-    return get_root_path("src", "linktools", *paths)
+from setuptools.command.editable_wheel import editable_wheel as _editable_wheel
 
 
 class ConsoleScripts(list):
@@ -60,82 +54,124 @@ class ConsoleScripts(list):
         return self
 
 
-if __name__ == '__main__':
+class ContextVar(property):
 
-    release = os.environ.get("RELEASE", "false").lower() == "true"
-    version = os.environ.get("VERSION", None)
-    if not version:
-        try:
-            with open(get_root_path(".version"), "rt", encoding="utf-8") as fd:
-                version = fd.read().strip()
-        except:
-            pass
-    if not version:
-        version = "0.0.1"
-    if version.startswith("v"):
-        version = version[len("v"):]
-    if not release:
-        items = []
-        for item in version.split("."):
-            find = re.findall(r"^\d+", item)
-            if find:
-                items.append(int(find[0]))
-        version = ".".join(map(str, items))
-        version = f"{version}.post100.dev0"
-
-    with open(get_src_path("develop", "metadata"), "rt", encoding="utf-8") as fd_in, \
-            open(get_src_path("metadata.py"), "wt", encoding="utf-8") as fd_out:
-        fd_out.write(
-            Template(fd_in.read()).render(
-                release=release,
-                version=version,
-                develop=False,
-            )
+    def __init__(self, key, default=None):
+        super().__init__(
+            fget=lambda o: o.get(key, default),
+            fset=lambda o, v: o.__setitem__(key, v)
         )
 
-    with open(get_src_path("develop", "tools.yml"), "rb") as fd_in, \
-            open(get_src_path("assets", "tools.json"), "wt") as fd_out:
-        json.dump(
-            {
+
+class Context(dict):
+    version: str = ContextVar("version")
+    release: bool = ContextVar("release")
+    develop: bool = ContextVar("develop")
+
+    def __init__(self):
+        super().__init__()
+        self.develop = False
+        self.release = os.environ.get("RELEASE", "false").lower() == "true"
+        self.version = self.parse_version()
+
+    @property
+    def root_path(self):
+        return pathlib.Path(os.path.dirname(__file__))
+
+    @property
+    def src_path(self):
+        return self.root_path / "src" / "linktools"
+
+    def parse_version(self):
+        version = os.environ.get("VERSION", None) or self._read_file(self.root_path / ".version")
+        if not version:
+            version = "0.0.1"
+        if version.startswith("v"):
+            version = version[len("v"):]
+        if not self.release:
+            items = []
+            for item in version.split("."):
+                find = re.findall(r"^\d+", item)
+                if find:
+                    items.append(int(find[0]))
+            version = ".".join(map(str, items))
+            version = f"{version}.post100.dev0"
+        return version
+
+    def parse_requires(self):
+        with open(self.root_path / "requirements.yml", "rt", encoding="utf-8") as fd:
+            data = yaml.safe_load(fd)
+            # install_requires = dependencies + dev-dependencies
+            install_requires = data.get("dependencies")
+            install_requires.extend(
+                data.get("release-dependencies") if self.release else data.get("dev-dependencies"))
+            # extras_require = optional-dependencies
+            extras_require = data.get("optional-dependencies")
+            all_requires = []
+            for requires in extras_require.values():
+                all_requires.extend(requires)
+            extras_require["all"] = all_requires
+        return install_requires, extras_require
+
+    def parse_scripts(self):
+        return ConsoleScripts().append_script(
+            script_name="lt",
+            module_name="linktools.__main__",
+        ).append_module(
+            self.src_path / "cli" / "commands" / "common",
+            module_prefix="linktools.cli.commands.common",
+            script_prefix="ct",
+        ).append_module(
+            self.src_path / "cli" / "commands" / "android",
+            module_prefix="linktools.cli.commands.android",
+            script_prefix="at",
+        ).append_module(
+            self.src_path / "cli" / "commands" / "ios",
+            module_prefix="linktools.cli.commands.ios",
+            script_prefix="it",
+        )
+
+    def rewrite_metadata(self):
+        with open(self.src_path / "develop" / "metadata", "rt", encoding="utf-8") as fd_in, \
+                open(self.src_path / "metadata.py", "wt", encoding="utf-8") as fd_out:
+            fd_out.write(Template(fd_in.read()).render(**self))
+
+    def rewrite_tools(self):
+        with open(self.src_path / "develop" / "tools.yml", "rb") as fd_in, \
+                open(self.src_path / "assets" / "tools.json", "wt") as fd_out:
+            json.dump({
                 key: value
                 for key, value in yaml.safe_load(fd_in).items()
                 if key[0].isupper()
-            },
-            fd_out
-        )
+            }, fd_out)
 
-    with open(get_root_path("requirements.yml"), "rt", encoding="utf-8") as fd:
-        data = yaml.safe_load(fd)
-        # install_requires = dependencies + dev-dependencies
-        install_requires = data.get("dependencies")
-        install_requires.extend(data.get("release-dependencies") if release else data.get("dev-dependencies"))
-        # extras_require = optional-dependencies
-        extras_require = data.get("optional-dependencies")
-        all_requires = []
-        for requires in extras_require.values():
-            all_requires.extend(requires)
-        extras_require["all"] = all_requires
+    def _read_file(self, path):
+        try:
+            with open(path, encoding="utf-8") as fd:
+                return fd.read().strip()
+        except:
+            return None
 
-    scripts = ConsoleScripts().append_script(
-        script_name="lt",
-        module_name="linktools.__main__",
-    ).append_module(
-        get_src_path("cli", "commands", "common"),
-        module_prefix="linktools.cli.commands.common",
-        script_prefix="ct",
-    ).append_module(
-        get_src_path("cli", "commands", "android"),
-        module_prefix="linktools.cli.commands.android",
-        script_prefix="at",
-    ).append_module(
-        get_src_path("cli", "commands", "ios"),
-        module_prefix="linktools.cli.commands.ios",
-        script_prefix="it",
-    )
+
+if __name__ == '__main__':
+    class EditableWheel(_editable_wheel):
+
+        def run(self):
+            context.develop = True
+            context.rewrite_metadata()
+            return super().run()
+
+
+    context = Context()
+    context.rewrite_metadata()
+    context.rewrite_tools()
+    install_requires, extras_require = context.parse_requires()
+    console_scripts = context.parse_scripts()
 
     setup(
-        version=version,
+        version=context.version,
         install_requires=install_requires,
         extras_require=extras_require,
-        entry_points={"console_scripts": scripts},
+        entry_points={"console_scripts": console_scripts},
+        cmdclass={"editable_wheel": EditableWheel},
     )
