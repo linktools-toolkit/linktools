72654a22c1d59b6894e73d59593e909d36a05908
ice-black-tea <30665081+ice-black-tea@users.noreply.github.com>
2026-09-05 08:18:53 +0800

    refactor(ai): narrow metrics observe surface

--- linktools-ai/src/linktools/ai/observe/__init__.py
+++ linktools-ai/src/linktools/ai/observe/__init__.py
@@ -4,7 +4,6 @@
 
 from ._memory import InMemoryMetricStore
 from ._metrics import Metrics
-from ._middleware import Middleware, MiddlewarePipeline
 from ._model import (
     MetricAggregation,
     MetricDefinition,
@@ -19,8 +18,6 @@ from ._model import (
     MetricWindow,
     Observation,
 )
-from ._scope import ObservationContext, context_for, current_context, reset_context, set_context
-from ._snapshot import RunSnapshot, snapshot_digest
 from ._sql import SqlMetricStore, build_metrics_sql_metadata, provision_metrics_database
 from ._sqlite import SQLiteMetricStore
 from ._store import MetricStore
@@ -45,13 +42,4 @@ __all__ = [
     "SqlMetricStore",
     "build_metrics_sql_metadata",
     "provision_metrics_database",
-    "Middleware",
-    "MiddlewarePipeline",
-    "ObservationContext",
-    "RunSnapshot",
-    "context_for",
-    "current_context",
-    "reset_context",
-    "set_context",
-    "snapshot_digest",
 ]
